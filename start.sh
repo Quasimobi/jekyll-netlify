b020a923585cabc25dd9729dc6aa202d65c35a35
Quasimobi <bauke.mobi@protonmail.com>
2024-10-03 11:44:50 +0200

    First commit 🎉

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+JEKYLL_ENV=development jekyll serve --livereload --port=4000
+
+
+
---
+++ xstart_01.sh
@@ -0,0 +1,1 @@
+/home/radiatus/Dropbox/bluefish/assets/lodash_bs4/default_bs4/default/scripts/xstart_01.sh
